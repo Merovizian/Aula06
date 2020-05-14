888ee7e0670aa8902b3859deb6a5624e83963ba6
Eric <ericgmicaela@gmail.com>
2020-05-13 21:33:42 -0300

    Versão 1.0 da aula 06

---
+++ Desafio003.py
@@ -0,0 +1,4 @@
+print("**Programa de soma entre dois numeros**\n")
+n1 = int(input("Digite o primeiro numero: "))
+n2 = int(input("Digite o segundo numero: "))
+print(f"\n{n1} + {n2} = {n1+n2}")
---
+++ Desafio004.py
@@ -0,0 +1,9 @@
+algo = input("Digite algo aqui: ")
+print("É um numero?",algo.isnumeric())
+print("É uma palavra?",algo.isalpha())
+print("É uma palavra com numeros?",algo.isalnum())
+print ("É com letras maiusculas?",algo.isupper())
+print ("É com letras minusculas?",algo.islower())
+print (type(algo))
+
+
